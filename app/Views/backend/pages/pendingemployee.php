c2ddd507f1107add538fab46b63b7499a494da56
leave1200 <markbarsaga121@gmail.com>
2024-10-14 03:41:38 +0800

    a

--- app/Views/backend/pages/pendingemployee.php
+++ app/Views/backend/pages/pendingemployee.php
@@ -6,12 +6,12 @@
 <div class="page-header">
     <div class="row">
         <div class="col-md-6 col-sm-12">
-            <div class="title">
-                <h4>Employee List</h4>
-            </div>
             <nav aria-label="breadcrumb" role="navigation">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="<?= route_to('admin.home'); ?>">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">
+                        Employee Approval List
+                    </li>
                 </ol>
             </nav>
         </div>
