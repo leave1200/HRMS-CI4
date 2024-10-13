e17f816e72143d664516fd74257f067127035bb0
leave1200 <markbarsaga121@gmail.com>
2024-10-14 03:40:05 +0800

    a

--- app/Views/backend/pages/pendingemployee.php
+++ app/Views/backend/pages/pendingemployee.php
@@ -23,7 +23,7 @@
 <div class="pd-20 card-box mb-30">
     <div class="clearfix mb-20">
         <div class="pull-left">
-            <h4 class="text-blue h4">Employee List</h4>
+            <h4 class="text-blue h4">Employee Pending List</h4>
         </div>
     </div>
     <div class="table-responsive">
