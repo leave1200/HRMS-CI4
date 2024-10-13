1b84bac4f979ec7583cceab1272f7dfd2011a6c0
leave1200 <markbarsaga121@gmail.com>
2024-10-13 15:24:01 +0800

    a

--- app/Views/backend/pages/auth/login.php
+++ app/Views/backend/pages/auth/login.php
@@ -97,6 +97,10 @@
         </div>
     </form>
 </div>
+<!-- Add SweetAlert CSS and JS in your layout file -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+
 
 
 <?= $this->endSection() ?>
