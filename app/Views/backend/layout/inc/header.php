46a4b34f8164c2ae8a13003b13f171d3d83ed606
leave1200 <markbarsaga121@gmail.com>
2024-10-18 01:49:52 +0800

    a

--- app/Views/backend/layout/inc/header.php
+++ app/Views/backend/layout/inc/header.php
@@ -127,17 +127,6 @@
                         <?php endif; ?>
                     </ul>
                     <ul class="list-group">
-                        <?php if (!empty($pendingEmployees)): ?>
-                            <?php foreach ($pendingEmployees as $employee): ?>
-                                <li class="list-group-item">
-                                    <a href="<?= route_to('admin.pendinglist') ?>">
-                                        <?= htmlspecialchars($employee['firstname'] . ' ' . $employee['lastname']) ?>
-                                    </a> has a pending result.
-                                </li>
-                            <?php endforeach; ?>
-                        <?php else: ?>
-                            <li class="list-group-item">No pending results.</li>
-                        <?php endif; ?>
                     </ul>
                 </div>
             </div>
