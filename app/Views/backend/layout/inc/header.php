1d30132c6a600db9ea01e81c1f96f37928f2d50a
leave1200 <markbarsaga121@gmail.com>
2024-10-18 01:25:14 +0800

    a

--- app/Views/backend/layout/inc/header.php
+++ app/Views/backend/layout/inc/header.php
@@ -90,6 +90,10 @@
 </style>
 
 <div class="header">
+	<div class="header-left">
+		<div class="menu-icon bi bi-list"></div>
+		<div class="header-search"></div>
+	</div>
 	<div class="header-right">
 		<div class="dashboard-setting user-notification">
 			<div class="dropdown">
@@ -146,6 +150,8 @@
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 <script>
 $(document).ready(function() {
