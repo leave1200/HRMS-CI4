467ca9253b4c12d1685510a327f9b71614629295
leave1200 <markbarsaga121@gmail.com>
2024-10-18 22:20:50 +0800

    a

--- app/Views/backend/pages/home.php
+++ app/Views/backend/pages/home.php
@@ -92,7 +92,7 @@
 							<div class="progress-box text-center">
 								<div style="display:inline;width:120px;height:120px;"><canvas width="150" height="150" style="width: 120px; height: 120px;"></canvas><input type="text" class="knob dial4" value="<?= $pendingCount ?>" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgcolor="#FAD6A5" data-fgcolor="#a683eb" data-angleoffset="180" readonly="readonly" style="width: 64px; height: 40px; position: absolute; vertical-align: middle; margin-top: 40px; margin-left: -92px; border: 0px; background: none rgb(255, 255, 255); font: bold 24px Arial; text-align: center; color: white; padding: 0px; appearance: none;font-size: 50px;">
 							</div>
-								<h5 class="text-white padding-top-10 h5">
+								<h5 class="text-white padding-top-10 h5"><i class="icon-copy fa fa-certificate" aria-hidden="true" style="font-size: 50px;"></i>
 									Attendance AM
 								</h5>
 							</div>
@@ -103,7 +103,7 @@
 							<div class="progress-box text-center">
 								<div style="display:inline;width:120px;height:120px;"><canvas width="150" height="150" style="width: 120px; height: 120px;"></canvas><input type="text" class="knob dial4" value="<?= $pendingCount ?>" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgcolor="#F15A9A" data-fgcolor="#a683eb" data-angleoffset="180" readonly="readonly" style="width: 64px; height: 40px; position: absolute; vertical-align: middle; margin-top: 40px; margin-left: -92px; border: 0px; background: none rgb(255, 255, 255); font: bold 24px Arial; text-align: center; color: white; padding: 0px; appearance: none;font-size: 50px;">
 							</div>
-								<h5 class="text-white padding-top-10 h5">
+								<h5 class="text-white padding-top-10 h5"><i class="icon-copy fa fa-circle" aria-hidden="true" style="font-size: 50px;"></i>
 									Attendance PM
 								</h5>
 							</div>
