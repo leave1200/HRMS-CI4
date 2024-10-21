6bcbf2c4420a912676656f46e7604fd0171255ae
leave1200 <markbarsaga121@gmail.com>
2024-10-22 01:04:01 +0800

    a

--- app/Views/backend/pages/leave_application.php
+++ app/Views/backend/pages/leave_application.php
@@ -123,7 +123,6 @@
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
 <script>
 $(document).ready(function() {
     $('#leaveApplicationsTable').DataTable({
