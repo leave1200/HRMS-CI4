5ffba7a3eecd171fc38f9811edd4288e60d123d3
leave1200 <markbarsaga121@gmail.com>
2024-10-22 01:04:17 +0800

    a

--- app/Views/backend/pages/leave_application.php
+++ app/Views/backend/pages/leave_application.php
@@ -120,8 +120,6 @@
         </div>
     </div>
 </div>
-
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
 <script>
 $(document).ready(function() {
