cd5dcda80162616db4e256b13be4768ce2969c78
leave1200 <markbarsaga121@gmail.com>
2024-10-22 01:57:54 +0800

    a

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -1348,7 +1348,7 @@ public function cancelHolidays()
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 public function approvedLeaves()
     {
-        $leaveModel = new LeaveModel();
+        $leaveModel = new LeaveApplicationModel();
         $data['approvedLeaves'] = $leaveModel->where('status', 'approved')->findAll();
         
         return view('admin/approved_leaves', $data); // Make sure to create this view file
