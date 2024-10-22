18368f1e6396b26e1535a4c522cacff885d1bba6
leave1200 <markbarsaga121@gmail.com>
2024-10-22 09:19:02 +0800

    a

--- app/Views/backend/pages/setting.php
+++ app/Views/backend/pages/setting.php
@@ -2,5 +2,15 @@
 <?= $this->section('content') ?>
 
 -------Settings content here---------
+<thead>
+            <tr>
+                <th>ID</th>
+                <th>Employee Name</th>
+                <th>Leave Type</th>
+                <th>Start Date</th>
+                <th>End Date</th>
+                <th>Status</th>
+            </tr>
+        </thead>
 
 <?= $this->endSection()?>
