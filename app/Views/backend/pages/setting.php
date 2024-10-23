edfd979ad98402b7f5c2de309f54be5d12252175
leave1200 <markbarsaga121@gmail.com>
2024-10-24 00:04:33 +0800

    a

--- app/Views/backend/pages/setting.php
+++ app/Views/backend/pages/setting.php
@@ -1,6 +1,24 @@
 <?= $this->extend('backend/layout/pages-layout') ?>
 <?= $this->section('content') ?>
 
--------Settings content here---------
+<div class="page-header">
+						<div class="row">
+							<div class="col-md-12 col-sm-12">
+								<div class="title">
+									<h4>Tabs</h4>
+								</div>
+								<nav aria-label="breadcrumb" role="navigation">
+									<ol class="breadcrumb">
+										<li class="breadcrumb-item">
+											<a href="index.html">Home</a>
+										</li>
+										<li class="breadcrumb-item active" aria-current="page">
+											UI Tabs
+										</li>
+									</ol>
+								</nav>
+							</div>
+						</div>
+					</div>
 
 <?= $this->endSection()?>
