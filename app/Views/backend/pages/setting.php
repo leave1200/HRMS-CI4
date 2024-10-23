a9a4f0a753b349578ad9edb549a03f004ddff0b4
leave1200 <markbarsaga121@gmail.com>
2024-10-24 00:05:48 +0800

    a

--- app/Views/backend/pages/setting.php
+++ app/Views/backend/pages/setting.php
@@ -5,12 +5,12 @@
 						<div class="row">
 							<div class="col-md-12 col-sm-12">
 								<div class="title">
-									<h4>Tabs</h4>
+									<h4>Setting</h4>
 								</div>
 								<nav aria-label="breadcrumb" role="navigation">
 									<ol class="breadcrumb">
 										<li class="breadcrumb-item">
-											<a href="index.html">Home</a>
+											<a href="<? route_to('admin.home') ?>">Home</a>
 										</li>
 										<li class="breadcrumb-item active" aria-current="page">
 											UI Tabs
