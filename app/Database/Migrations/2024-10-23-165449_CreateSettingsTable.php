7fe6649d4f5ac91614264fa4fda3d98030ec214e
leave1200 <markbarsaga121@gmail.com>
2024-10-24 01:41:56 +0800

    a

--- app/Database/Migrations/2024-10-23-165449_CreateSettingsTable.php
+++ app/Database/Migrations/2024-10-23-165449_CreateSettingsTable.php
@@ -8,11 +8,53 @@ class CreateSettingsTable extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            's_id'=>[
+                'type'=>'INT',
+                'unsigned'=>true,
+                'auto_increment'=>true,
+            ],
+            'title'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>'255'
+            ],
+            'email'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>'255'
+            ],
+            'phone'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>'255',
+                'null'=>true,
+            ],
+            'meta_keywords'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>'255',
+                'null'=>true,
+            ],
+            'description'=>[
+                'type'=>'TEXT',
+                'null'=>true,
+            ],
+            'logo'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>'255',
+                'null'=>true,
+            ],
+            'favicon'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>'255',
+                'null'=>true,
+            ],
+             'created_at timestamp default current_timestamp',
+              'updated_at timestamp default current_timestamp on update current_timestamp',
+        ]);
+        $this->forge->addKey('s_id', true);
+        $this->forge->createTable('settings');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('settings');
     }
 }
