5ff1c326a4de80aac564ec915b825665609a2aee
leave1200 <markbarsaga121@gmail.com>
2024-10-25 01:37:51 +0800

    a

--- app/Views/backend/pages/auth/recaptcha_form.php
+++ app/Views/backend/pages/auth/recaptcha_form.php
@@ -8,7 +8,7 @@
 <script src="https://www.google.com/recaptcha/api.js?render=6LfaHGsqAAAAAO2c4GXxqpOPKhxeTRqQ7FkVeF4m"></script>
 <script>
     grecaptcha.ready(function() {
-        grecaptcha.execute('6LfaHGsqAAAAAO2c4GXxqpOPKhxeTRqQ7FkVeF4mY', { action: 'login' }).then(function(token) {
+        grecaptcha.execute('6LfaHGsqAAAAAO2c4GXxqpOPKhxeTRqQ7FkVeF4mY', { action: 'loginForm' }).then(function(token) {
             document.getElementById('recaptcha_token').value = token;
         });
     });
