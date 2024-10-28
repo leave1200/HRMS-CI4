89ca60f3b555f3f00e9ce5f87a3e3a88b8fd8250
leave1200 <markbarsaga121@gmail.com>
2024-10-29 04:24:44 +0800

    a

--- app/Views/backend/pages/attendance_report.php
+++ app/Views/backend/pages/attendance_report.php
@@ -150,6 +150,45 @@
         </div>
     </div>
 </div>
+<!-- Archived Records Modal -->
+<div class="modal fade" id="archiveModal" tabindex="-1" role="dialog" aria-labelledby="archiveModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="archiveModalLabel">Archived Attendance Records</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="table-responsive">
+                    <table class="table table-striped" id="archivedTable">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Date</th>
+                                <th>Name</th>
+                                <th>Office</th>
+                                <th>Position</th>
+                                <th>AM Sign In</th>
+                                <th>AM Sign Out</th>
+                                <th>PM Sign In</th>
+                                <th>PM Sign Out</th>
+                            </tr>
+                        </thead>
+                        <tbody id="archivedData">
+                            <!-- Archived records will be populated here -->
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <!-- Add SweetAlert2 CDN -->
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
