1772db75aa5386f505b1c609e2b0b181b2e76cfd
leave1200 <markbarsaga121@gmail.com>
2024-10-29 23:11:18 +0800

    a

--- app/Views/backend/pages/holidays.php
+++ app/Views/backend/pages/holidays.php
@@ -10,7 +10,7 @@
 			</div>
 			<nav aria-label="breadcrumb" role="navigation">
 				<ol class="breadcrumb">
-					<li class="breadcrumb-item"><a href="<?= base_url('index') ?>">Home</a></li>
+					<li class="breadcrumb-item"><a href="<?= route_to('admin.home') ?>">">Home</a></li>
 					<li class="breadcrumb-item active" aria-current="page">Holidays</li>
 				</ol>
 			</nav>
--- app/Views/backend/pages/leave_type.php
+++ app/Views/backend/pages/leave_type.php
@@ -9,7 +9,7 @@
             </div>
             <nav aria-label="breadcrumb" role="navigation">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                    <li class="breadcrumb-item"><a href="<?= route_to('admin.home') ?>">Home</a></li>
                     <li class="breadcrumb-item active" aria-current="page">Leave Type</li>
                 </ol>
             </nav>
