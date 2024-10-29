825ab4587ca389b92829690e695d2bf7ea5f5022
leave1200 <markbarsaga121@gmail.com>
2024-10-30 01:40:43 +0800

    a

--- app/Views/backend/pages/employee.php
+++ app/Views/backend/pages/employee.php
@@ -207,9 +207,7 @@
 							</div>
 						</div>
                     </div>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
 <script>
     document.getElementById('dob').addEventListener('change', function() {
         const dobInput = this.value;
