feba3f5dc2de7874a2c99531e3487dcdcf3d8b38
leave1200 <markbarsaga121@gmail.com>
2024-10-30 06:20:52 +0800

    a

--- app/Views/backend/pages/employeelist.php
+++ app/Views/backend/pages/employeelist.php
@@ -167,8 +167,8 @@
                                                             <input type="text" id="view_dob" class="form-control" readonly>
                                                         </div>
                                                     </div>
-                                                    <div class="col-md-10">
-                                                        <div class="form-group">
+                                                </div>
+                                                <div class="form-group">
                                                             <label for="view_age">Age</label>
                                                             <input type="text" id="view_age" class="form-control" readonly>
                                                         </div>
@@ -176,8 +176,6 @@
                                                             <label for="view_sex">Sex</label>
                                                             <input type="text" id="view_sex" name="sex" class="form-control" readonly>
                                                         </div>
-                                                    </div>
-                                                </div>
                                                 <div class="form-group">
                                                             <label for="view_address">Address</label>
                                                             <input type="text" id="view_address" class="form-control" readonly>
