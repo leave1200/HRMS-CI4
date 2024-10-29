1a550548f535bde08ccd4ad420917f17a0cf9538
leave1200 <markbarsaga121@gmail.com>
2024-10-30 04:44:33 +0800

    a

--- app/Views/backend/pages/employee.php
+++ app/Views/backend/pages/employee.php
@@ -59,7 +59,7 @@
                             <div class="col-md-6">
                                 <div class="form-group">
                                 <label for="address">Address :</label>
-                                <input type="text" class="form-control" id="address" name="address" required/>
+                                <input type="text" class="form-control" id="address" name="address" oninput="this.value = this.value.replace(/[^A-Za-z\s,.]/g, '')" required/>
                                 </div>
                             </div>
                             <div class="col-md-6">
