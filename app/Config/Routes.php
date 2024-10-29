81a8ba835b87122fe3d2bd55e1b27000928c2f86
leave1200 <markbarsaga121@gmail.com>
2024-10-29 10:48:46 +0800

    a

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -77,7 +77,7 @@ $routes->group('admin', static function($routes){
         $routes->post('attendance/delete', 'AdminController::deleteAttendance', ['as' => 'attendance.delete']);
         $routes->post('attendance/archive', 'AdminController::archiveAttendance', ['as' => 'attendance.archive']);
         $routes->get('attendance/archived', 'AdminController::archived', ['as' => 'attendance.archived']);
-        $routes->post('attendance/pm-signin', 'AdminController::pmSignIn', ['as' => 'attendance_pm_save']);
+        $routes->post('attendance/pm-signin', 'AdminController::pmSignIn', ['as' => 'attendance.pm_save']);
 
 
 
