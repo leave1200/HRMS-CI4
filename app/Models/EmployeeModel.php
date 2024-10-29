56642453608293cb0c3e001e0a1793745454c897
leave1200 <markbarsaga121@gmail.com>
2024-10-30 06:25:16 +0800

    a

--- app/Models/EmployeeModel.php
+++ app/Models/EmployeeModel.php
@@ -8,7 +8,7 @@ class EmployeeModel extends Model
 {
     protected $table = 'employee'; // Make sure this matches your table name
     protected $primaryKey = 'id';
-    protected $allowedFields = ['firstname', 'lastname', 'email', 'phone', 'dob','address','p_school', 's_school', 't_school', 'interview_for', 'interview_type', 'interview_date', 'interview_time', 'behaviour', 'result', 'comment','picture','sex'];
+    protected $allowedFields = ['firstname', 'lastname', 'email', 'phone', 'dob','address','p_school', 's_school', 't_school', 'interview_for', 'interview_type', 'interview_date', 'interview_time', 'behaviour', 'result', 'comment','picture','sex', 'age'];
     public function getEmployeeNames()
     {
         return $this->select('id, firstname, lastname, email, picture')->findAll(); // Include email in the selection
