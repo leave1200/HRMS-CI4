7c69198f0cc27233f15f78f53bfa066aab7157a7
leave1200 <markbarsaga121@gmail.com>
2024-10-31 14:55:40 +0800

    a

--- app/Views/backend/pages/profile.php
+++ app/Views/backend/pages/profile.php
@@ -176,6 +176,11 @@
         </div>
     </div>
 </div>
+<!-- Include Bootstrap CSS/JS and jQuery in your HTML head -->
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 
 <script>
