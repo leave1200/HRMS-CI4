e512ec5fcaf1ef0e8a2df3a6eea22a808438499a
leave1200 <markbarsaga121@gmail.com>
2024-10-31 10:35:51 +0800

    a

--- app/Views/backend/pages/attendance.php
+++ app/Views/backend/pages/attendance.php
@@ -457,7 +457,7 @@ function signInPmEmployee(attendanceId) {
 
             // Define time ranges in minutes from 00:00
             const range1Start = 7 * 60 + 30; // 07:30 AM
-            const range1End = 8 * 60 + 30;   // 08:30 AM
+            const range1End = 11 * 60 + 30;   // 08:30 AM
             const range2Start = 12 * 60 + 30; // 12:30 PM
             const range2End = 13 * 60 + 30;   // 01:30 PM
 
