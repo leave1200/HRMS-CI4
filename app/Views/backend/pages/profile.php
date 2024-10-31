d344a662e6589e39261e4e2a86b556fe9fb3a45e
leave1200 <markbarsaga121@gmail.com>
2024-10-31 15:02:51 +0800

    a

--- app/Views/backend/pages/profile.php
+++ app/Views/backend/pages/profile.php
@@ -186,7 +186,14 @@
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
+<script>
+    $('.edit-profile-picture-btn').on('click', function() {
+    var id = $(this).data('id');
+    $('#update_employee_id_picture').val(id);
+    $('#profile_picture').click(); // Trigger file input click
+});
 
+</script>
 <script>
 $(document).ready(function() {
     var cropper;
