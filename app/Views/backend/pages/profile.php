a9f08a08165ff4346f89804c625c8358f469902e
leave1200 <markbarsaga121@gmail.com>
2024-11-03 01:22:58 +0800

    a

--- app/Views/backend/pages/profile.php
+++ app/Views/backend/pages/profile.php
@@ -197,22 +197,22 @@
 
 
     $('#user_profile_file').ijaboCropTool({
-    preview: '.ci-avatar-photo',
-    setRatio: 1,
-    allowedExtensions: ['jpg', 'jpeg', 'png'],
-    processUrl: '<?= route_to('update-profile-picture') ?>',
-    withCSRF: ['<?= csrf_token() ?>', '<?= csrf_hash() ?>'],
-    onSuccess:function(responseText, element, status) {
-        if( status == 1 ) {
-            toastr.success('message');
-        } else {
-            toastr.error('message');
+        preview: '.ci-avatar-photo',
+        setRatio: 1,
+        allowedExtensions: ['jpg', 'jpeg', 'png'],
+        processUrl:'<?= route_to('update-profile-picture') ?>',
+        withCSRF: ['<?= csrf_token() ?>', '<?= csrf_hash() ?>'],
+        onSuccess:function(responseText, element, status) {
+            if( status == 1 ) {
+                toastr.success('message');
+            } else {
+                toastr.error('message');
+            }
+        },
+        onError: function(message, element, status) {
+            alert(message);
         }
-    },
-    onError: function(message, element, status) {
-        alert(message);
-    }
-});
+    });
 
 $('#change_password_form').on('submit', function(e){
     e.preventDefault();
