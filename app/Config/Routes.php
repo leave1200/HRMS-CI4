7e2cee0c0ab78b7af6dd5c92d72cb45ac8d6c8bd
leave1200 <markbarsaga121@gmail.com>
2024-11-03 01:41:08 +0800

    a

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -97,8 +97,6 @@ $routes->group('', static function($routes){
     });
     $routes->group('Profile-page', ['filter'=>'cifilter:auth'], static function($routes){
 
-                //$routes->view('example-page','example-page');
-                $routes->get('home', 'AdminController::index', ['as' => 'admin.home']);
                 $routes->get('logout', 'AdminController::logoutHandler', ['as' => 'admin.logout']);
                 ///////profile
                 $routes->get('profile','AdminController::profile',['as'=>'admin.profile']);
