79fbef7d7aa48c7b325d9618dbab84e0000763c7
leave1200 <markbarsaga121@gmail.com>
2024-11-08 17:50:27 +0800

    a

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,7 +36,7 @@ class User extends Model
         'email'             => 'required|valid_email',
         'password'          => 'required|min_length[8]',
         'confirm_password'  => 'matches[password]',
-        'picture'           => 'mime_in[picture,image/jpg,image/jpeg,image/png]|max_size[picture,2048]',
+        // 'picture'           => 'mime_in[picture,image/jpg,image/jpeg,image/png]|max_size[picture,2048]',
         'bio'               => 'permit_empty|string',
         'status'            => 'required|string|max_length[255]'
     ];
