443fa65efd7dd6dcf1b8b01ae45b1d2ba40cd370
leave1200 <markbarsaga121@gmail.com>
2024-11-10 15:27:34 +0800

    aa

--- app/Helpers/CIMail_helper.php
+++ app/Helpers/CIMail_helper.php
@@ -4,30 +4,39 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-/** email sending logic */
-if(!function_exists('sendEmail') ){
-    function sendEmail($mailConfig){
+if (!function_exists('sendEmail')) {
+    function sendEmail($mailConfig) {
         require 'PHPMailer/src/Exception.php';
         require 'PHPMailer/src/PHPMailer.php';
         require 'PHPMailer/src/SMTP.php';
 
         $mail = new PHPMailer(true);
-        $mail->SMTPDebug = 0;
-        $mail->isSMTP();
-        $mail->Host = env('EMAIL_HOST');
-        $mail->SMTPAuth = true;
-        $mail->Username = env('EMAIL_USERNAME');
-        $mail->Password = env('EMAIL_PASSWORD');
-        $mail->SMTPSecure = env('EMAIL_ENCRYPTION');
-        $mail->Port = env('EMAIL_PORT');
-        $mail->setFrom($mailConfig['mail_from_email'],$mailConfig['mail_from_name']);
-        $mail->addAddress($mailConfig['mail_recipient_email'],$mailConfig['mail_recipient_name']);
-        $mail->isHTML(true);
-        $mail->Subject = $mailConfig['mail_subject'];
-        $mail->Body = $mailConfig['mail_body'];
-        if( $mail->send() ){
+
+        try {
+            // Server settings
+            $mail->isSMTP();
+            $mail->Host = env('EMAIL_HOST');
+            $mail->SMTPAuth = true;
+            $mail->Username = env('EMAIL_USERNAME');
+            $mail->Password = env('EMAIL_PASSWORD');
+            $mail->SMTPSecure = env('EMAIL_ENCRYPTION');
+            $mail->Port = env('EMAIL_PORT');
+
+            // Recipient settings
+            $mail->setFrom($mailConfig['mail_from_email'], $mailConfig['mail_from_name']);
+            $mail->addAddress($mailConfig['mail_recipient_email'], $mailConfig['mail_recipient_name']);
+
+            // Content settings
+            $mail->isHTML(true);
+            $mail->Subject = $mailConfig['mail_subject'];
+            $mail->Body = $mailConfig['mail_body'];
+
+            // Send email
+            $mail->send();
             return true;
-        }else{
+        } catch (Exception $e) {
+            // Log the error or handle it as needed
+            error_log('Mail error: ' . $mail->ErrorInfo);
             return false;
         }
     }
