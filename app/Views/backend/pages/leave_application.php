8227e3832714a4435cfd14af826460685880f7f5
leave1200 <markbarsaga121@gmail.com>
2024-11-19 07:56:51 +0800

    a

--- app/Views/backend/pages/leave_application.php
+++ app/Views/backend/pages/leave_application.php
@@ -30,20 +30,11 @@
                 <div class="form-group row">
                     <label class="col-sm-12 col-md-2 col-form-label">Name</label>
                     <div class="col-sm-12 col-md-10">
-                        <select name="la_name" class="form-control" required>
-                            <option value="" disabled selected>Select Employee</option>
-                            <?php if (!empty($employees) && is_array($employees)): ?>
-                                <?php foreach ($employees as $employee): ?>
-                                    <option value="<?= esc($employee['id']) ?>">
-                                        <?= esc($employee['firstname'] . ' ' . $employee['lastname']) ?>
-                                    </option>
-                                <?php endforeach; ?>
-                            <?php else: ?>
-                                <option value="" disabled>No employees available</option>
-                            <?php endif; ?>
-                        </select>
+                        <input type="text" class="form-control" value="<?= esc($userName) ?>" readonly>
+                        <input type="hidden" name="user_id" value="<?= session()->get('user_id') ?>">
                     </div>
                 </div>
+
 
 
                 <div class="form-group row">
