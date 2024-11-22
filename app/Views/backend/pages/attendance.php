3cfab79b78d5d783c656f34cdf3b02003b2de124
leave1200 <markbarsaga121@gmail.com>
2024-11-22 23:18:09 +0800

    a

--- app/Views/backend/pages/attendance.php
+++ app/Views/backend/pages/attendance.php
@@ -98,7 +98,6 @@
                                     <th>Position</th>
                                     <th>AM Sign In</th>
                                     <th>Sign Out</th>
-                                    <th>ATT</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -109,7 +108,6 @@
                                             <td><?= esc($attendance['name']) ?></td>
                                             <td><?= esc($attendance['office']) ?></td>
                                             <td><?= esc($attendance['position']) ?></td>
-                                            <td><?= esc($attendance['att']) ?></td>
                                             
                                             <!-- AM Sign Out status -->
                                             <td>
