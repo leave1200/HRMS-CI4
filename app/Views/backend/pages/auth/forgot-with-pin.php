9f9e87c6191dabac0fbf635f63e0598c1c8a80cf
leave1200 <markbarsaga121@gmail.com>
2024-11-23 03:25:23 +0800

    a

--- app/Views/backend/pages/auth/forgot-with-pin.php
+++ app/Views/backend/pages/auth/forgot-with-pin.php
@@ -1,6 +1,24 @@
 <?= $this->extend('backend/layout/auth-layout') ?>
 <?= $this->section('content') ?>
 
--------auth content here---------
+<form action="<?= route_to('send-pin-code') ?>" method="POST">
+    <?= csrf_field(); ?>
+
+    <?php if (!empty(session()->getFlashdata('success'))): ?>
+        <div class="alert alert-success">
+            <?= session()->getFlashdata('success'); ?>
+        </div>
+    <?php endif; ?>
+
+    <div class="input-group custom">
+        <input type="email" class="form-control" placeholder="Enter your email" name="email">
+        <div class="input-group-append">
+            <span class="input-group-text"><i class="fa fa-envelope-o"></i></span>
+        </div>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Send Pin</button>
+</form>
+
 
 <?= $this->endSection()?>
