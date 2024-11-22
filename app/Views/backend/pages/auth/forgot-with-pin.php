007a6f77fc8aa675594deedae3093ffacb6a63cd
leave1200 <markbarsaga121@gmail.com>
2024-11-23 03:29:23 +0800

    a

--- app/Views/backend/pages/auth/forgot-with-pin.php
+++ app/Views/backend/pages/auth/forgot-with-pin.php
@@ -27,11 +27,6 @@
 
     <button type="submit" class="btn btn-primary">Send Pin</button>
 </form>
-<div class="col-5">
-										<div class="input-group mb-0">
-											<a class="btn btn-outline-primary btn-lg btn-block" href="<?= route_to('admin.login.form') ?>">Login</a>
-										</div>
-									</div>
 </div>
 
 
