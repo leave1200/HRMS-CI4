ebc3197071fc5abb19c65b84a4b56f52e171a5fd
leave1200 <markbarsaga121@gmail.com>
2024-11-28 23:59:47 +0800

    a

--- app/Views/backend/layout/inc/header.php
+++ app/Views/backend/layout/inc/header.php
@@ -174,7 +174,6 @@
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 <script>
-$(document).ready(function() {
   $(document).ready(function() {
     function fetchPendingNotifications() {
     $.ajax({
