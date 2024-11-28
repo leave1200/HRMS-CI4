4daa0b8e05bcf630ef6a11988925ace175e05995
leave1200 <markbarsaga121@gmail.com>
2024-11-29 00:28:45 +0800

    a

--- app/Views/backend/layout/inc/header.php
+++ app/Views/backend/layout/inc/header.php
@@ -127,22 +127,22 @@
 </a>
 
 
-<div class="dropdown-menu dropdown-menu-right notifications-dropdown">
-            <h6 class="dropdown-header">Notifications</h6>
-            <ul class="list-group">
-                <?php if (!empty($pendingEmployees) && session()->get('userStatus') !== ''): ?>
-                    <?php foreach ($pendingEmployees as $employee): ?>
-                        <li class="list-group-item">
-                            <a href="<?= route_to('admin.pendinglist') ?>">
-                                <?= htmlspecialchars($employee['firstname'] . ' ' . $employee['lastname']) ?>
-                            </a> has a pending result.
-                        </li>
-                    <?php endforeach; ?>
-                <?php else: ?>
-                    <li class="list-group-item">No pending Employee.</li>
-                <?php endif; ?>
-            </ul>
-        </div>
+          <div class="dropdown-menu dropdown-menu-right notifications-dropdown">
+              <h6 class="dropdown-header">Notifications</h6>
+              <ul class="list-group">
+                  <?php if (!empty($pendingEmployees)): ?>
+                      <?php foreach ($pendingEmployees as $employee): ?>
+                          <li class="list-group-item">
+                              <a href="<?= route_to('admin.pendinglist') ?>">
+                                  <?= htmlspecialchars($employee['firstname'] . ' ' . $employee['lastname']) ?>
+                              </a> has a pending result.
+                          </li>
+                      <?php endforeach; ?>
+                  <?php else: ?>
+                      <li class="list-group-item">No pending Employee.</li>
+                  <?php endif; ?>
+              </ul>
+          </div>
       </div>
   </div>
 
