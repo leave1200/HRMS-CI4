c2b82d9805642e0d5b1f2427587143367b4dce85
leave1200 <markbarsaga121@gmail.com>
2024-11-29 21:04:27 +0800

    a

--- app/Libraries/CIAuth.php
+++ app/Libraries/CIAuth.php
@@ -24,6 +24,7 @@ class CIAuth
             return null;
         }
     }
+
     public static function check(){
         $session = session();
         return $session->has('logged_in');
@@ -31,19 +32,24 @@ class CIAuth
 
     public static function forget(){
         $session = session();
+        // Remove session data
         $session->remove('logged_in');
         $session->remove('userdata');
+
+        // Destroy the ci_session cookie
+        setcookie('ci_session', '', time() - 3600, '/', '', true, true); // Expire the cookie immediately
+
+        // Optionally, you can also destroy any other cookies like csrf_cookie_name
+        setcookie('csrf_cookie_name', '', time() - 3600, '/', '', true, true); // Expire CSRF cookie if needed
     }
 
     public static function user(){
         $session = session();
         if( $session->has('logged_in') ){
             if( $session->has('userdata') ){
-               // return $session->get('userdata');
-
+               // return the user data from the session or database
                $user = new User();
                 return $user->asObject()->where('id',CIAuth::id())->first();
-
             }else{
                 return null;
             }
