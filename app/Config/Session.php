180888af19695b6526700e0dfbe141cdd3307daa
leave1200 <markbarsaga121@gmail.com>
2024-11-29 21:21:19 +0800

    a

--- app/Config/Session.php
+++ app/Config/Session.php
@@ -30,7 +30,7 @@ class Session extends BaseConfig
      *
      * The session cookie name, must contain only [0-9a-z_-] characters
      */
-    public string $cookieName = '';
+    public string $cookieName = 'ci_session';
 
     /**
      * --------------------------------------------------------------------------
