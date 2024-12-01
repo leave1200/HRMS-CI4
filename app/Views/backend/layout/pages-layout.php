f4bb5b9a13cdc37ed77f2e7021c7bbc789dcd855
leave1200 <markbarsaga121@gmail.com>
2024-12-02 02:33:45 +0800

    a

--- app/Views/backend/layout/pages-layout.php
+++ app/Views/backend/layout/pages-layout.php
@@ -75,14 +75,12 @@
 		<script src="/backend/vendors/scripts/script.min.js"></script>
 		<script src="/backend/vendors/scripts/process.js"></script>
 		<script src="/backend/vendors/scripts/layout-settings.js"></script>
-		<!-- <script src="/backend/src/plugins/apexcharts/apexcharts.min.js"></script> -->
 		<script src="/backend/src/plugins/jquery-steps/jquery.steps.js"></script>
 		<script src="/backend/vendors/scripts/steps-setting.js"></script>
 		<script src="/backend/src/plugins/datatables/js/jquery.dataTables.min.js"></script>
 		<script src="/backend/src/plugins/datatables/js/dataTables.bootstrap4.min.js"></script>
 		<script src="/backend/src/plugins/datatables/js/dataTables.responsive.min.js"></script>
 		<script src="/backend/src/plugins/datatables/js/responsive.bootstrap4.min.js"></script>
-		<!-- <script src="/backend/vendors/scripts/dashboard3.js"></script> -->
 		<script src="/extra-assets/ijaboCropTool/ijaboCropTool.min.js"></script>
 		<script src="/backend/src/plugins/sweetalert2/sweetalert2.all.js"></script>
 		<script src="/backend/src/plugins/sweetalert2/sweet-alert.init.js"></script>
