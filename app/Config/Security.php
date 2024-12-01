abcf97264e15f1ebbcb60f016d3261f6933069ea
leave1200 <markbarsaga121@gmail.com>
2024-12-02 03:19:54 +0800

    a

--- app/Config/Security.php
+++ app/Config/Security.php
@@ -15,7 +15,7 @@ class Security extends BaseConfig
      *
      * @var string 'cookie' or 'session'
      */
-    public string $csrfProtection = 'csrf_cookie_name';
+    public string $csrfProtection = 'none';
 
     /**
      * --------------------------------------------------------------------------
