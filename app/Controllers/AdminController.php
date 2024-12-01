f4cfd33eb74af50390550f285d8768083b1444bb
leave1200 <markbarsaga121@gmail.com>
2024-12-01 23:54:30 +0800

    a

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -185,6 +185,7 @@ public function getUserLeaveApplications()
 
 
     public function logoutHandler(){
+        $this->userModel->update($userInfo['id'], ['policy' => 'Online']);
         CIAuth::forget();
         return redirect()->route('admin.login.form')->with('fail', 'You are logged out!');
         delete_cookie('csrf_cookie_name');  // Make sure this matches the cookie name you are using
