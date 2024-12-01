273d7fdb1ddca37c6bb517cc6d59b8f18836dbc8
leave1200 <markbarsaga121@gmail.com>
2024-12-02 03:04:38 +0800

    a

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -22,8 +22,9 @@ class AuthController extends BaseController
     }
     public function loginForm()
     {
-        delete_cookie('NID', '.google.com', '/');;
-        delete_cookie('ci_session', 'hrmo-lawis.com', '/');
+        delete_cookie('NID', '.google.com', '/');
+        delete_cookie('_GRECAPTCHA', '.google.com', '/recaptcha');
+        delete_cookie('ci_session', '/', '/');
         delete_cookie('debug-bar-state', 'hrmo-lawis.com', '/');
         // Assume we have a function to check system availability
         if (!$this->isSystemAccessible()) {
