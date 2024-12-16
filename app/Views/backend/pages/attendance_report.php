018dbc4ff3845df2ad60d32ab0f7b320454069ae
leave1200 <markbarsaga121@gmail.com>
2024-12-17 00:23:05 +0800

    a

--- app/Views/backend/pages/attendance_report.php
+++ app/Views/backend/pages/attendance_report.php
@@ -308,8 +308,8 @@ function printDataTable() {
                 <div style="width: 48%; border: 1px solid #000; padding: 10px;">
                 <p style="text-align: left;"><i>Civil Service Form No. 48</i></p>
                     <h3 style="text-align: center;">DAILY TIME RECORD</h3>
-                    <p style="text-align: center;">${monthYearText}</p>
                     <p><strong>Name:</strong> ${name}</p>
+                    <p style="text-align: center;">${monthYearText}</p>
                     <p><strong>Regular Days:</strong> ${numberOfDays}</p>
                     <p><strong>Saturdays:</strong> ${numberOfSaturdays}</p>
                     <table border="1" cellpadding="5" cellspacing="0" style="width: 100%; text-align: center; border-collapse: collapse;">
@@ -344,8 +344,8 @@ function printDataTable() {
                 <div style="width: 48%; border: 1px solid #000; padding: 10px;">
                 <p style="text-align: left;"><i>Civil Service Form No. 48</i></p>
                     <h3 style="text-align: center;">DAILY TIME RECORD</h3>
-                    <p style="text-align: center;">${monthYearText}</p>
                     <p><strong>Name:</strong> ${name}</p>
+                    <p style="text-align: center;">${monthYearText}</p>
                     <p><strong>Regular Days:</strong> ${numberOfDays}</p>
                     <p><strong>Saturdays:</strong> ${numberOfSaturdays}</p>
                     <table border="1" cellpadding="5" cellspacing="0" style="width: 100%; text-align: center; border-collapse: collapse;">
