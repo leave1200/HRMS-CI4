5fe96ad308305afc625859e009069b896219e5f0
leave1200 <markbarsaga121@gmail.com>
2024-12-17 01:20:48 +0800

    a

--- app/Views/backend/pages/employeelist.php
+++ app/Views/backend/pages/employeelist.php
@@ -2,6 +2,8 @@
 
 <?= $this->extend('backend/layout/pages-layout') ?>
 <?= $this->section('content') ?>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 
 <div class="page-header">
     <div class="row">
