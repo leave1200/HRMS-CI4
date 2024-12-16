c5b56c649f88e772f17a4ae11c0d9d82b7ee3cc2
leave1200 <markbarsaga121@gmail.com>
2024-12-17 01:16:45 +0800

    a

--- app/Views/backend/pages/employeelist.php
+++ app/Views/backend/pages/employeelist.php
@@ -974,10 +974,10 @@ function validateDesignation1(input) {
 <script>
     $(document).ready(function () {
         $('#DataTables_Table_0').DataTable({
-            "pageLength": 10, // Show 10 entries per page
+            "pageLength": 9, // Show 10 entries per page
             "lengthChange": false, // Disable the option to change page size
             "searching": true, // Enable search functionality
-            "order": [[1, "asc"]] // Order by the first column (ID) ascending
+            "order": [[0, "asc"]] // Order by the first column (ID) ascending
         });
     });
 </script>
