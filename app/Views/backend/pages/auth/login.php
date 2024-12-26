20f6b075b9004b059000db1de269a0ce7cab81f5
leave1200 <markbarsaga121@gmail.com>
2024-12-26 12:09:48 +0800

    a

--- app/Views/backend/pages/auth/login.php
+++ app/Views/backend/pages/auth/login.php
@@ -8,97 +8,97 @@
 
     <?php $validation = \Config\Services::validation(); ?>
 
-    <form action="<?= route_to('admin.login.handler') ?>" method="POST">
-        <?= csrf_field() ?> <!-- Ensuring CSRF protection is in place -->
+<form action="<?= esc(route_to('admin.login.handler'), 'attr') ?>" method="POST">
+    <?= csrf_field() ?> <!-- Ensuring CSRF protection is in place -->
 
-        <!-- Success flash message -->
-        <?php if (!empty(session()->getFlashdata('success'))) : ?>
-            <div class="alert alert-success">
-                <?= esc(session()->getFlashdata('success')) ?>
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-        <?php endif; ?>
+    <!-- Success flash message -->
+    <?php if (!empty(session()->getFlashdata('success'))) : ?>
+        <div class="alert alert-success">
+            <?= esc(session()->getFlashdata('success')) ?>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    <?php endif; ?>
 
-        <!-- Failure flash message -->
-        <?php if (!empty(session()->getFlashdata('fail'))) : ?>
-            <div class="alert alert-danger">
-                <?= esc(session()->getFlashdata('fail')) ?>
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-        <?php endif; ?>
+    <!-- Failure flash message -->
+    <?php if (!empty(session()->getFlashdata('fail'))) : ?>
+        <div class="alert alert-danger">
+            <?= esc(session()->getFlashdata('fail')) ?>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    <?php endif; ?>
 
-        <!-- System Inaccessible Alert -->
-        <?php if (session()->get('system_accessible') === false): ?>
-            <script>
-                swal({
-                    title: "System Unavailable!",
-                    text: "The system is currently inaccessible. Please try again later.",
-                    icon: "warning",
-                    button: "OK"
-                });
-            </script>
-        <?php endif; ?>
-        <input type="hidden" name="recaptcha_token" id="recaptcha_token">
-        <!-- Input for Username or Email -->
-        <div class="input-group custom">
-            <input type="text" class="form-control form-control-lg" placeholder="Username or Email" name="login_id" value="<?= esc(set_value('login_id')) ?>">
-            <div class="input-group-append custom">
-                <span class="input-group-text"><i class="icon-copy dw dw-user1"></i></span>
+    <!-- System Inaccessible Alert -->
+    <?php if (session()->get('system_accessible') === false): ?>
+        <script>
+            swal({
+                title: "System Unavailable!",
+                text: "The system is currently inaccessible. Please try again later.",
+                icon: "warning",
+                button: "OK"
+            });
+        </script>
+    <?php endif; ?>
+
+    <!-- Input for Username or Email -->
+    <div class="input-group custom">
+        <input type="text" class="form-control form-control-lg" placeholder="Username or Email" name="login_id" value="<?= esc(set_value('login_id')) ?>">
+        <div class="input-group-append custom">
+            <span class="input-group-text"><i class="icon-copy dw dw-user1"></i></span>
+        </div>
+    </div>
+    
+    <!-- Validation Error for login_id -->
+    <?php if ($validation->getError('login_id')): ?>
+        <div class="d-block text-danger" style="margin-top: 25px; margin-bottom: 15px;">
+            <?= esc($validation->getError('login_id')) ?>
+        </div>
+    <?php endif; ?>
+
+    <!-- Input for Password -->
+    <div class="input-group custom">
+        <input type="password" class="form-control form-control-lg" placeholder="**********" name="password" value="<?= esc(set_value('password')) ?>">
+        <div class="input-group-append custom">
+            <span class="input-group-text"><i class="dw dw-padlock1"></i></span>
+        </div>
+    </div>
+
+    <!-- Validation Error for password -->
+    <?php if ($validation->getError('password')): ?>
+        <div class="d-block text-danger" style="margin-top: 25px; margin-bottom: 15px;">
+            <?= esc($validation->getError('password')) ?>
+        </div>
+    <?php endif; ?>
+
+    <div class="row pb-30">
+        <div class="col-6">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="customCheck1" name="remember">
+                <label class="custom-control-label" for="customCheck1">Remember</label>
             </div>
         </div>
-        
-        <!-- Validation Error for login_id -->
-        <?php if ($validation->getError('login_id')): ?>
-            <div class="d-block text-danger" style="margin-top: 25px; margin-bottom: 15px;">
-                <?= esc($validation->getError('login_id')) ?>
-            </div>
-        <?php endif; ?>
-
-        <!-- Input for Password -->
-        <div class="input-group custom">
-            <input type="password" class="form-control form-control-lg" id="password" placeholder="**********" name="password" value="<?= esc(set_value('password')) ?>">
-            <div class="input-group-append custom">
-                <span class="input-group-text" id="togglePassword">
-                    <i class="dw dw-padlock1"></i>
-                </span>
+        <div class="col-6">
+            <div class="forgot-password">
+                <a href="<?= esc(route_to('admin.forget.forms'), 'attr') ?>">Forgot Password</a>
             </div>
         </div>
+    </div>
 
+    <!-- Google reCAPTCHA v3 Hidden Token Field -->
+    <input type="hidden" name="recaptcha_token" id="recaptcha_token">
 
-        <!-- Validation Error for password -->
-        <?php if ($validation->getError('password')): ?>
-            <div class="d-block text-danger" style="margin-top: 25px; margin-bottom: 15px;">
-                <?= esc($validation->getError('password')) ?>
-            </div>
-        <?php endif; ?>
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="input-group mb-0">
-                <!-- <input class="btn btn-primary btn-lg btn-block" type="submit" value="Sign In" onclick="submitForm()"> -->
-                <input class="btn btn-primary btn-lg btn-block" type="submit" value="Sign In">
-                </div>
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="input-group mb-0">
+            <!-- <input class="btn btn-primary btn-lg btn-block" type="submit" value="Sign In" onclick="submitForm()"> -->
+            <input class="btn btn-primary btn-lg btn-block" type="submit" value="Sign In">
             </div>
         </div>
-        <div class="row pb-30">
-            <div class="col-6">
-                <div class="custom-control custom-checkbox">
-                    <!-- <input type="checkbox" class="custom-control-input" id="customCheck1" name="remember">
-                    <label class="custom-control-label" for="customCheck1">Remember</label> -->
-                </div>
-            </div>
-            <div class="col-6">
-                <div class="forgot-password">
-                    <a href="<?= esc(route_to('admin.forgot.form'), 'attr') ?>">Forgot Password</a>
-                </div>
-            </div>
-        </div>
-
-
-    </form>
+    </div>
+</form>
 </div>
 <!-- <script src="https://www.google.com/recaptcha/api.js?render=6LdcqXoqAAAAAOKpdE7btEsLljApBKTl9gLoj5Ct"></script>
 <script>
