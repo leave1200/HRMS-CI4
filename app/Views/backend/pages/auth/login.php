3c24fa0c87390f4bd55ce09f2b0a5ba64618db13
leave1200 <markbarsaga121@gmail.com>
2024-12-26 13:47:28 +0800

    a

--- app/Views/backend/pages/auth/login.php
+++ app/Views/backend/pages/auth/login.php
@@ -100,10 +100,10 @@
 
     </form>
 </div>
-<script src="https://www.google.com/recaptcha/api.js?render=6LeIE6YqAAAAANev7MxAZoB6vIIPhWoSBkCsa4TA"></script>
+<script src="https://www.google.com/recaptcha/api.js?render=6fLdcqXoqAAAAAOKpdE7btEsLljApBKTl9gLoj5Ct"></script>
 <script>
     grecaptcha.ready(function() {
-        grecaptcha.execute('6LeIE6YqAAAAANev7MxAZoB6vIIPhWoSBkCsa4TA', { action: 'login' }).then(function(token) {
+        grecaptcha.execute('6LfdcqXoqAAAAAOKpdE7btEsLljApBKTl9gLoj5Ct', { action: 'login' }).then(function(token) {
             document.getElementById('recaptcha_token').value = token;
         });
     });
